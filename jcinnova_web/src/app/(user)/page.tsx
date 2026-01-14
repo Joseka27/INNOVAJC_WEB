6761a7d6b7e36101848fbbbb7d8d458941397950
Jose Carlos Cubero <josecarlos.cubero.r@gmail.com>
2026-01-13 22:30:44 -0600

    Parte 2 de la pagina Principal avance

--- jcinnova_web/src/app/(user)/page.tsx
+++ jcinnova_web/src/app/(user)/page.tsx
@@ -45,23 +45,84 @@ const Home = () => {
             </div>
           </div>
         </div>
-        <div className="pg_main-presentation-part2"></div>
+
+        <div className="pg_main-presentation-part2">
+          <div className="pg_main-part2-sections">
+            <div className="pg_main-part2-section1">
+              <h3>Porqué Nosotros?</h3>
+              <div className="pg_main-part2-section1-about">
+                <Image
+                  className="imagelogo"
+                  src="/images/grafics.png"
+                  alt="Logo"
+                  width={100}
+                  height={50}
+                ></Image>
+                <div className="pg_main-part2-section1-textbox">
+                  <h3>Desarrollo a la Medida</h3>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Autem, tempore sint aut error, praesentium voluptatem
+                    doloribus iste dolorum consectetur alias laborum! Doloremque
+                    doloribus minus ut quis sapiente totam distinctio maxime.
+                  </p>
+                </div>
+              </div>
+              <hr></hr>
+              <div className="pg_main-part2-section1-about">
+                <Image
+                  className="imagelogo"
+                  src="/images/grafics.png"
+                  alt="Logo"
+                  width={100}
+                  height={50}
+                ></Image>
+                <div className="pg_main-part2-section1-textbox">
+                  <h3>Dispositivos Móviles</h3>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. A
+                    odit ratione quidem accusantium consequuntur illo ad atque
+                    nobis, consectetur aliquid neque quisquam commodi libero
+                    iure natus maxime culpa nihil itaque!
+                  </p>
+                </div>
+              </div>
+              <hr></hr>
+              <div className="pg_main-part2-section1-about">
+                <Image
+                  className="imagelogo"
+                  src="/images/grafics.png"
+                  alt="Logo"
+                  width={100}
+                  height={50}
+                ></Image>
+                <div className="pg_main-part2-section1-textbox">
+                  <h3>Sistema Administrativo Contable</h3>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Temporibus est debitis beatae harum aperiam suscipit ratione
+                    saepe ipsa incidunt. Aliquam velit quidem, iusto aspernatur
+                    exercitationem quo eligendi facere placeat ad.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="pg_main-part2-section2">
+              <Image
+                className="imagelogo"
+                src="/images/grafics.png"
+                alt="Logo"
+                width={100}
+                height={50}
+              ></Image>
+            </div>
+          </div>
+        </div>
+
         <div className="pg_main-presentation-part3"></div>
         <div className="pg_main-presentation-part4"></div>
-        <div className="pg_main-presentation-part5">
-          <h1>a</h1>
-          <h1>a</h1>
-          <h1>a</h1>
-          <h1>a</h1>
-          <h1>a</h1>
-          <h1>a</h1>
-          <h1>a</h1>
-          <h1>a</h1>
-          <h1>a</h1>
-          <h1>a</h1>
-          <h1>a</h1>
-          <h1>a</h1>
-        </div>
+        {/* Va de otro color */}
+        <div className="pg_main-presentation-part5"></div>
         <div className="pg_main-presentation-part6">
           <div className="pg-main-joinus">
             <h1>
